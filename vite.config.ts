056c0a99a200996470c921d907dbc92c38e2d598
Bergbok <66174189+Bergbok@users.noreply.github.com>
2026-09-11 19:47:20 +0200

    feat: change PWA icon background from red to black

--- vite.config.ts
+++ vite.config.ts
@@ -78,7 +78,7 @@ export default defineConfig({
 				description:
 					'Gongo is a 2D side-scrolling platformer where the player goes through levels killing enemies and saving the princess. Each level has amazing hand-picked worlds. On each of the levels, a princess is hidden, which must be rescued and delivered to your castle, cleaning up enemies along the way.',
 				theme_color: '#fffc3f',
-				background_color: '#d41212',
+				background_color: '#000000',
 				display: 'standalone'
 			},
 			pwaAssets: {
